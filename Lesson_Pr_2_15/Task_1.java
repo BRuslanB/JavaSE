e8cf3e94109e33992211544515810a223d4a98db
BRuslanB <103117752+BRuslanB@users.noreply.github.com>
2022-05-20 00:48:12 +0600

    Update Task_1.java

--- Lesson_Pr_2_15/Task_1.java
+++ Lesson_Pr_2_15/Task_1.java
@@ -1,16 +1,13 @@
-package Lesson_Dz_1_17;
-
 import java.util.Scanner;
-public class Task_1 {
+public class Main{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        System.out.println(chech_div_2(n));
-    }
-    public static String chech_div_2(int a){
-        if (a%2==0)
-            return "YES";
-        return "NO";
-
+        int a = in.nextInt();
+        int b = in.nextInt();
+        if (a>b) {
+            System.out.println(a);
+        }else{
+            System.out.println(b);
+        }
     }
 }
