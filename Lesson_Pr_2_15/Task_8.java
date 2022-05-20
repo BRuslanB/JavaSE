e0a2df88e2c7f7ebdeb1a9dbfa0e860df3a76dd4
BRuslanB <103117752+BRuslanB@users.noreply.github.com>
2022-05-20 18:44:27 +0600

    Update Task_8.java

--- Lesson_Pr_2_15/Task_8.java
+++ Lesson_Pr_2_15/Task_8.java
@@ -1,25 +1,16 @@
-package Lesson_Dz_1_17;
-
+/*
+Дано число. Если оно меньше или равно 2019, то вывести NO, если больше или равно 2021, то вывести YES, если равно 2020, то вывести ERROR.
+*/
 import java.util.Scanner;
-public class Task_8 {
+public class Main{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int m = in.nextInt();
-        int[][] mas = new int[n][m];
-        for (int i=0;i<n; i++)
-            for (int j=0;j<m; j++)
-                mas[i][j] = in.nextInt();
-        max_mas(mas);
-    }
-    public static void max_mas(int[][] mas1){
-        int max;
-        for (int i=0; i<mas1.length;i++) {
-            max=mas1[i][0];
-            for (int j=1; j<mas1[i].length;j++)
-                if (mas1[i][j] > max)
-                    max = mas1[i][j];
-            System.out.println(max);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        if (a+100==b || a-100==b) {
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
         }
     }
 }
