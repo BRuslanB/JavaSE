38928d18fe3cd9045f809840e6c4796b957b664a
BRuslanB <103117752+BRuslanB@users.noreply.github.com>
2022-05-20 18:45:12 +0600

    Update Task_9.java

--- Lesson_Pr_2_15/Task_9.java
+++ Lesson_Pr_2_15/Task_9.java
@@ -1,19 +1,16 @@
-package Lesson_Dz_1_17;
-
+/*
+Создайте программу, которая поможет Арсену быстро выучить названия 12-ти месяцев на английском. Арсен будет вводит порядковый номер того или иного месяца в консоль, а далее программу будет выводить наименование с заглавной буквы как в примерах ниже, но а если такого месяца не существует, то выводит Wrong Input.
+*/
 import java.util.Scanner;
-public class Task_9 {
+public class Main{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int[] mas = new int[n];
-        for (int i=0;i<n; i++) {
-            mas[i] = in.nextInt();
+        int a = in.nextInt();
+        if (a>20) {
+            a=a/6;
+        }else{
+            a=a*6;
         }
-        choose_mas(mas);
-    }
-    public static void choose_mas(int[] mas1){
-        for (int i=0; i<mas1.length;i++)
-            if (mas1[i]<50 && mas1[i]%5==0)
-                System.out.println(mas1[i]);
+        System.out.println(a);
     }
 }
