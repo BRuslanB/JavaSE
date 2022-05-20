a7b8fde5a0103762d6547b1344849172c38ab646
BRuslanB <103117752+BRuslanB@users.noreply.github.com>
2022-05-20 17:55:05 +0600

    Update Task_2.java

--- Lesson_Pr_2_15/Task_2.java
+++ Lesson_Pr_2_15/Task_2.java
@@ -1,6 +1,6 @@
 /*
 Напишите программу, которая проверяет делиться ли значение введенное с консоли на 17. Если значение делится, программа выводит DIVISIBLE BY 17, иначе ничего выводить не надо.
-/*
+*/
 import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
