748b6eb8f64072dd06b32355267307b4ff82e058
BRuslanB <103117752+BRuslanB@users.noreply.github.com>
2022-05-20 18:48:46 +0600

    Update Task_13.java

--- Lesson_Pr_2_15/Task_13.java
+++ Lesson_Pr_2_15/Task_13.java
@@ -1,24 +1,3 @@
-package Lesson_Dz_1_17;
-
-import java.util.Scanner;
-public class Task_13 {
-    public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int[] mas = new int[n];
-        for (int i=0;i<n; i++)
-            mas[i] = in.nextInt();
-        int m = in.nextInt();
-        change_sum(mas,m);
-    }
-    public static void change_sum(int[] mas1, int m1){
-        int sum=0;
-        for (int i=0; i<mas1.length;i++)
-            sum+=mas1[i];
-        for (int i=0; i<mas1.length;i++) {
-            if (mas1[i] > m1)
-                mas1[i] = sum;
-            System.out.print(mas1[i] + " ");
-        }
-    }
-}
+/*
+Напишите программу, которая будет принимать целое число с консоли. Далее программа должна проверить, если число меньше 100 или больше 200, то выводить YES, иначе NO.
+*/
