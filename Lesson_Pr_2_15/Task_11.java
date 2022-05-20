95f8e7b9d6a9bb4bdabf1b9d755b1c867080771d
BRuslanB <103117752+BRuslanB@users.noreply.github.com>
2022-05-20 18:47:23 +0600

    Update Task_11.java

--- Lesson_Pr_2_15/Task_11.java
+++ Lesson_Pr_2_15/Task_11.java
@@ -1,20 +1,16 @@
-package Lesson_Dz_1_17;
-
+/*
+Создайте программу, которая выводит в консоль YES, если введенное целое число является положительным и четным, иначе NO.
+*/
 import java.util.Scanner;
-public class Task_11 {
+public class Main{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        String[] mas = new String[n];
-        for (int i=0;i<n; i++)
-            mas[i] = in.next();
-        String s = in.next();
-        System.out.println(incude_string(mas,s));
-    }
-    public static String incude_string(String[] mas1, String s1){
-        for (int i=0; i<mas1.length; i++)
-            if (mas1[i].indexOf(s1) !=-1)
-                return "YES";
-        return "NO";
+        int a = in.nextInt();
+        int b = in.nextInt();
+        if (a%b==0) {
+            System.out.println("divisible");
+        }else{
+            System.out.println("not divisible");
+        }
     }
 }
