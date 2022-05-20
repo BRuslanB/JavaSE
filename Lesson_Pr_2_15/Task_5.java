96d185f901a636f966135bf9d7b8f8e4fd15b1c5
BRuslanB <103117752+BRuslanB@users.noreply.github.com>
2022-05-20 17:56:57 +0600

    Update Task_5.java

--- Lesson_Pr_2_15/Task_5.java
+++ Lesson_Pr_2_15/Task_5.java
@@ -1,27 +1,28 @@
-package Lesson_Dz_1_17;
-
+/*
+Напишите программу, где ввожу целое число n, и если оно четное, программа выведет EVEN иначе ODD.
+*/
 import java.util.Scanner;
-public class Task_5 {
+public class Main{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int[] mas = new int[n];
-        for (int i=0;i<n; i++) {
-            mas[i] = in.nextInt();
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int d = in.nextInt();
+        int max = a; //System.out.println("a="+a+ " max="+ max);
+        if (b>max) {
+            max=b; //System.out.println("b="+b+ " max="+ max);
         }
-        uniq_mas(mas);
-    }
-    public static void uniq_mas(int[] mas1){
-        boolean f;
-        for (int i=0; i<mas1.length;i++) {
-            f = true;
-            for (int j = 0; j < mas1.length; j++)
-                if (mas1[i] == mas1[j] && i!=j) {
-                    f = false;
-                    break;
-                }
-            if (f)
-                System.out.print(mas1[i]+" ");
+        if (c>max){
+            max=c; //System.out.println("c="+c+ " max="+ max);
+        }
+        if (d>max){
+            max=d; //System.out.println("d="+d+ " max="+ max);
+        }
+        if (max%2 == 0) {
+            System.out.println(max);
+        }else{
+            System.out.println("Not Found");
         }
     }
 }
