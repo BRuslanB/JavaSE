b0d550440470c2e9672b54d13839b9442967ffcb
BRuslanB <103117752+BRuslanB@users.noreply.github.com>
2022-05-20 18:46:44 +0600

    Update Task_10.java

--- Lesson_Pr_2_15/Task_10.java
+++ Lesson_Pr_2_15/Task_10.java
@@ -1,31 +1,18 @@
-package Lesson_Dz_1_17;
-
+/*
+Напишите программу, где я ввожу три числа. Если все числа одинаковые, то выведите YES, иначе NO.
+*/
 import java.util.Scanner;
-public class Task_10 {
+public class Main{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         int a = in.nextInt();
-        System.out.println(season(a));
-    }
-    public static String season(int x){
-        switch (x){
-            case 1:
-            case 2:
-            case 12:
-                return "Winter";
-            case 3:
-            case 4:
-            case 5:
-                return "Spring";
-            case 6:
-            case 7:
-            case 8:
-                return "Summer";
-            case 9:
-            case 10:
-            case 11:
-                return "Autumn";
+        if (a>0) {
+            a++;
+        }else if (a<0) {
+            a=a-2;
+        }else{
+            a=10;
         }
-        return "";
+        System.out.println(a);
     }
 }
